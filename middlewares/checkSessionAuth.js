dfcc2f9bed732c5ace68e58cc8e1439bbb7e2364
shahzaib777 <fa18-bse-097@cuilahore.edu.pk>
2021-06-22 22:31:51 +0500

    update

--- middlewares/checkSessionAuth.js
+++
@@ -1,7 +0,0 @@
-function checkSessionAuth(req, res, next) {
-  //set variable for every pug file
-  if (req.session.user) next();
-  else return res.redirect("/login");
-}
-
-module.exports = checkSessionAuth;
--- middlewares/sessionAuth.js
+++
@@ -1,7 +0,0 @@
-function sessionAuth(req, res, next) {
-  //set variable for every pug file
-  res.locals.user = req.session.user;
-  next();
-}
-
-module.exports = sessionAuth;
